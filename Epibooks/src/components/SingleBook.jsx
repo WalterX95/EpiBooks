7f73b12e4839b940b44564f582c33cb8ffb83d91
WalterX95 <walter_1995@hotmail.it>
2025-01-09 16:58:14 +0100

    Aggiunta 2 componenti

--- Epibooks/src/components/SingleBook.jsx
+++ Epibooks/src/components/SingleBook.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { Card, ListGroup, Container } from "react-bootstrap";
-import book from '../data/fantasy.json';
+import libro from '../data/fantasy.json';
 
 class SingleBook extends Component {
   render() {
@@ -9,30 +9,27 @@ class SingleBook extends Component {
               fluid={true}
               className='d-flex flex-wrap justify-content-center mt-5'
             >
-              {book.map(book => {
-                return (
-                  <Card style={{ width: '16rem' }} key={book.asin} className='mx-2 mb-2'>
+                  <Card style={{ width: '16rem' }} key={this.props.libro.asin} className='mx-2 mb-2'>
                     <Card.Img
                       variant='top'
-                      src={book.img}
+                      src={libro.img}
                       style={{ height: '400px' }}
                       className='w-100 object-fit-cover'
                     />
                     <Card.Body className='d-flex flex-column justify-content-between'>
-                      <Card.Title className='fw-bold'>{book.title}</Card.Title>
-                      <Card.Text className='ms-auto'>€{book.price}</Card.Text>
+                      <Card.Title className='fw-bold'>{this.props.libro.title}</Card.Title>
+                      <Card.Text className='ms-auto'>€{this.props.libro.price}</Card.Text>
                     </Card.Body>
                     <ListGroup className='list-group-flush text-center'>
                       <ListGroup.Item className='fs-5 fw-bold'>
-                        {book.category}
+                        {this.props.libro.category}
                       </ListGroup.Item>
                       <ListGroup.Item className='fs-6'>
-                        ASIN: {book.asin}
+                        ASIN: {this.props.libro.asin}
                       </ListGroup.Item>
                     </ListGroup>
                   </Card>
-                );
-              })}
+
             </Container>
           );
      }
